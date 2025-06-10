404d3c68d60152471b6b8469a0ef6bf6b9901a62
Parinay Rana <parinayrana@gmail.com>
2025-06-10 15:27:33 +0530

    Ingestion changes

--- app.py
+++ app.py
@@ -5,12 +5,23 @@ import sys
 from src.LungCancerDetection.components.data_ingestion import DataIngestion
 from src.LungCancerDetection.components.data_ingestion import DataIngestionConfig
 
+from src.LungCancerDetection.components.data_transformation import DataTransformationConfig
+from src.LungCancerDetection.components.data_transformation import DataTransformation
+from src.LungCancerDetection.components.data_transformation import DateTransformationExtractor
+
 if __name__ =='__main__':
     logging.info("the executions has started")
 
     try:
+        #data_ingestion_config = DataIngestionConfig()
         data_ingestion = DataIngestion()
-        data_ingestion.initiate_data_ingestion()
+        train_data_path, test_data_path = data_ingestion.initiate_data_ingestion()
+
+        data_transform_config = DataTransformationConfig()
+        data_transform = DataTransformation()
+        data_transform.initiate_data_transformation(train_data_path, test_data_path)
+
+        
 
     except Exception as e:
         logging.info("Custom Exception")
--- src/LungCancerDetection/components/data_ingestion.py
+++ src/LungCancerDetection/components/data_ingestion.py
@@ -32,6 +32,11 @@ class DataIngestion:
             train_set.to_csv(self.ingestion_config.train_data_path,index=False, header=True)
             test_set.to_csv(self.ingestion_config.test_data_path,index=False, header=True)
 
+            logging.info('Data Ingestion Complete')
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
             
         except Exception as e:
             raise CustomException(e,sys)
