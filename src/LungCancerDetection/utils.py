b69a5b44efe9a9992a6a53ce379724f258c6e6a5
Parinay Rana <parinayrana@gmail.com>
2025-06-24 17:49:58 +0530

    utils update:

--- src/LungCancerDetection/utils.py
+++ src/LungCancerDetection/utils.py
@@ -9,6 +9,8 @@ from src.LungCancerDetection.logger import logging
 import pandas as pd
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, classification_report
+from sklearn.calibration import CalibratedClassifierCV
+from sklearn.metrics import roc_auc_score
 
 
 def save_object(file_path, obj):
@@ -34,18 +36,30 @@ def evaluate_models(X_train,y_train, X_test, y_test, models, params):
             gs = GridSearchCV(model, para, cv=3)
             gs.fit(X_train,y_train)
 
-            model.set_params(**gs.best_params_)
-            model.fit(X_train,y_train)
+            best_model = gs.best_estimator_
 
-            y_train_pred = model.predict(X_train)
+            clf = CalibratedClassifierCV(base_estimator=best_model,cv=5, method="isotonic" )
+            clf.fit(X_train,y_train)
 
-            y_test_pred = model.predict(X_test)
+            y_train_pred = clf.predict_proba(X_train)
 
-            f1_train = f1_score(y_train,y_train_pred)
+            y_test_pred = clf.predict_proba(X_test)[:, 1]
 
-            f1_test = f1_score(y_test, y_test_pred)
+            # model.set_params(**gs.best_params_)
+            # model.fit(X_train,y_train)
 
-            report[list(models.keys())[i]] = f1_test
+            # y_train_pred = model.predict(X_train)
+
+            # y_test_pred = model.predict(X_test)
+
+            roc_train = roc_auc_score(y_train,y_train_pred)
+
+            #f1_train = f1_score(y_train,y_train_pred)
+
+            #f1_test = f1_score(y_test, y_test_pred)
+            roc_test = roc_auc_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = roc_test
 
         return report
     
